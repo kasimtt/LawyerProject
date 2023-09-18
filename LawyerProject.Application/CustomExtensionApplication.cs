a46b17acf8d5b75b9810b0327d48b6612ece3c05
KASIM İSLAM TATLI <202802055@ogr.cbu.edu.tr>
2023-09-18 19:44:54 +0300

    CustomExtensions added

---
+++ LawyerProject.Application/CustomExtensionApplication.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerProject.Application
+{
+    public static class CustomExtensionApplication
+    {
+        public static void AddContainerWithDependenciesApplication(this IServiceCollection services)
+        {
+            //buraya validationlar girilecek panpa
+
+        }
+    }
+}
---
+++ LawyerProject.Persistence/CustomExtensionPersistence.cs
@@ -0,0 +1,38 @@
+﻿using LawyerProject.Application.Repositories.AdvertRepositories;
+using LawyerProject.Application.Repositories.CaseRepositories;
+using LawyerProject.Application.Repositories.UserActivityRepositories;
+using LawyerProject.Application.Repositories.UserRepositories;
+using LawyerProject.Persistence.Repositories.AdvertRepositories;
+using LawyerProject.Persistence.Repositories.CaseRepositories;
+using LawyerProject.Persistence.Repositories.UserActivityRepositories;
+using LawyerProject.Persistence.Repositories.UserRepositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerProject.Persistence
+{
+    public static class CustomExtensionPersistence
+    {
+        public static void AddContainerWithDependenciesPersistence(this IServiceCollection services)
+        {
+
+            services.AddScoped<IUserReadRepository, UserReadRepository>();
+            services.AddScoped<IUserWriteRepository, UserWriteRepository>();
+
+            services.AddScoped<IAdvertReadRepository, AdvertReadRepository>();
+            services.AddScoped<IAdvertWriteRepository, AdvertWriteRepository>();
+
+            services.AddScoped<IUserActivityReadRepository, UserActivityReadRepository>();
+            services.AddScoped<IUserActivityWriteRepository, UserActivityWriteRepository>();
+
+            services.AddScoped<ICaseReadRepository,CaseReadRepository>();
+            services.AddScoped<ICaseWriteRepository,CaseWriteRepository>();
+
+
+        }
+    }
+}
