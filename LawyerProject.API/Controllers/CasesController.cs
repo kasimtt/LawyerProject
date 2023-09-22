135633b0df916a09d8d64e76ffec92d491d87bb9
KASIM İSLAM TATLI <202802055@ogr.cbu.edu.tr>
2023-09-22 20:05:28 +0300

    fixed some bug

--- LawyerProject.API/Controllers/CasesController.cs
+++ LawyerProject.API/Controllers/CasesController.cs
@@ -26,10 +26,10 @@ namespace LawyerProject.API.Controllers
 
 
         [HttpGet("getall")]
-        public IActionResult Get([FromQuery]Pagination pagination)
+        public IActionResult Get([FromQuery] Pagination pagination)
         {
             // throw new Exception("hata var kardaş hele bak buraya");
-            var totalCount= _caseReadRepository.GetAll().Count();
+            var totalCount = _caseReadRepository.GetAll().Count();
             var result = _caseReadRepository.GetAll().Skip(pagination.Size * pagination.Page).Take(pagination.Size);
             // gecici bunu yapıyorum daha sonra bu işlemler service classlarına alınacak haberin olsun
             IEnumerable<GetCasesDto> entityDto = _mapper.Map<IEnumerable<Case>, IEnumerable<GetCasesDto>>(result);
@@ -40,12 +40,12 @@ namespace LawyerProject.API.Controllers
                 entityDto,
             });
         }
-         
+
         [HttpPost("Add")]
         public IActionResult Post([FromBody] CreateCaseDto createCaseDto) // şimdilik case şeklinde gönderiyoruz bunları CQRS pattern'e göre düzenlicez
         {
             Case _case = _mapper.Map<Case>(createCaseDto);
-           var result= _caseWriteRepository.Add(_case);
+            var result = _caseWriteRepository.Add(_case);
             _caseWriteRepository.Save();
 
             if (result)
@@ -53,6 +53,20 @@ namespace LawyerProject.API.Controllers
                 return Ok("başarıyla kaydedildi panpa");
             }
             return BadRequest("kaydetme başarısız");
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Put([FromBody] UpdateCaseDto updateCaseDto)
+        {
+            Case _case = _mapper.Map<Case>(updateCaseDto);
+            var result = _caseWriteRepository.Update(_case);
+            _caseWriteRepository.Save();
+            if (result)
+            {
+                return Ok("başarıyla güncellendi panpa");
+            }
+            return BadRequest("Güncelleme yapılamadı");
+
         }
     }
 }
